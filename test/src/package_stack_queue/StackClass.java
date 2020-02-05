f81b27d3db250627f48fa3ed420fdef689f4e605
GressWu <1162925512@qq.com>
2020-02-05 17:08:09 +0800

    Learning makes man wise

---
+++ test/src/package_stack_queue/StackClass.java
@@ -0,0 +1,17 @@
+package package_stack_queue;
+
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+
+public class StackClass {
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack<>();
+        while (!StdIn.isEmpty())
+        {
+            st.push(StdIn.readInt());
+        }
+        for (Integer integer : st) {
+            System.out.println(integer);
+        }
+    }
+}
